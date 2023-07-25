a9bed634cf81bc807d14688caf0b2bb8e7999c23
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-26 00:31:01 +0700

    Update border

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -6,6 +6,7 @@ class CurrencyConverter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -15,6 +16,22 @@ class CurrencyConverter extends StatelessWidget {
               style: TextStyle(
                 fontSize: 45,
                 fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: "Please input the amount in USD",
+                hintStyle: TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
               ),
             ),
           ],
